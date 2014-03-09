e034a85fbb63680ddaa1fa55a65e28febeb7da5f
Sean Ballinger <ballinger@spitzer.ap.columbia.edu>
2014-03-08 22:29:07 -0500

    working for FB01S2P

--- vf_tree.py
+++ vf_tree.py
@@ -15,7 +15,7 @@ f = open('./coil_R_Z','r')
 ((OHR,OHZ),(VFR,VFZ),(SHR,SHZ)) = pickle.load(f)
 f.close()
 
-"""FB01_S2P data."""
+"""FB01_S2P specs."""
 pos_x = -0.51261392
 pos_y = -0.92477993
 pos_z = -0.077077734
@@ -38,25 +38,17 @@ vf_time = node_vf.dim_of().data()
 (sensor_time, sensor_signal) = clip(sensor_time, sensor_signal)
 (vf_time, vf_signal) = clip(vf_time, vf_signal)
 
-# """Create empty array for flux values."""
-# flux_vals = []
-# for i in range(len(vf_signal)):
-# 	flux_vals.append(0)
-
 """Calculate flux."""
-flux_vals = flux_val_calc(vf_signal, VFR, VFZ, sens_r, pos_z, sens_len, sens_width, sens_z_norm, coils)
-# for i_coil in range(len(VFR)-1):
-#     i_sig = 0
-#     for current in vf_signal:
-#         flux_vals[i_sig] += coils*sens_z_norm*(frac_flux(sens_len, current, sens_r, pos_z, VFR[i_coil], VFZ[i_coil]) - frac_flux(sens_len, current, sens_r-sens_width, pos_z, VFR[i_coil], VFZ[i_coil]))
-#         i_sig += 1
+# field_vals = sensor_flux_calc(vf_signal, VFR, VFZ, sens_r, pos_z, sens_len, sens_width, sens_z_norm, coils)
+theta = acos(0.87206924)
+field_vals = green_times_current(vf_signal, VFR, VFZ, sens_r, pos_z, theta)
 
 """Calculate average ratio between sensor signal and calculated flux."""
 i = 0
 ratios_sum = 0
 scaling_threshold = 0.001
 ratios = []
-for f in flux_vals:
+for f in field_vals:
 	if sensor_signal[i] > scaling_threshold:
 		ratio = sensor_signal[i]/f
 		ratios.append(ratio)
@@ -69,24 +61,38 @@ for i in sensor_time:
 	scaling_vals.append(scaling_threshold)
 
 print '------------------------'
-print 'Average scaling factor between calculated and measured flux: %s' % avg_ratio
+print 'Measured field = %.6s * calculated field' % avg_ratio
 print 'Calculated only using values above %s' % scaling_threshold
 print '------------------------'
 
-"""Multiply flux values by inductance."""
-for i in range(len(flux_vals)):
-	flux_vals[i] *= avg_ratio
+"""Multiply field values by inductance."""
+for i in range(len(field_vals)):
+	field_vals[i] *= -1
 
 def plot_flux():
 	plt.figure(1)
-	plt.plot(vf_time, flux_vals, label='calculated flux')
+
+	plt.subplot(211)
+	plt.plot(vf_time, field_vals, label='calculated field')
 	plt.plot(sensor_time, sensor_signal, label='sensor data')
 	plt.plot(sensor_time, scaling_vals, label='scaling threshold')
+	plt.xlim([0,.06])
+	plt.ylabel('Tesla')
 	plt.legend()
+
+	plt.subplot(212)
+	plt.plot(vf_time, vf_signal, label='vf current')
+	plt.xlim([0,.06])
+	plt.ylabel('A')
+	plt.xlabel('s')
+	plt.legend()
+
 	plt.show()
 
 endTime = time.time()
 elapsed = endTime-startTime
 print '%.2f seconds elapsed.' % elapsed
+
+print '%f' % green_base()
 
 plot_flux()
